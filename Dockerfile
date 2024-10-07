81ddd612f7c919f773a726581ff02514a92e055b
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2024-10-07 19:28:26 +0100

    PECI branch

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,9 @@ RUN apt update \
 && rm -rf /var/lib/apt/lists/* \
 && pip install -r requirements.txt
 
+# env vars
+ENV OMPI_ALLOW_RUN_AS_ROOT=1
+ENV OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
 CMD ["tail", "-f", "/dev/null"]
 
--- Programs/config.py
+++ Programs/config.py
@@ -13,21 +13,14 @@ from Utils.DatasetUtils import DatasetUtils
 from Utils.MLUtils import MLUtils
 from Utils.FLUtils import FLUtils
 from Utils.ModelUtils import ModelUtils
-from Utils.XAIUtils import XAIUtils
 
 # datasets
 from Datasets.IOT_DNL import IOT_DNL
-from Datasets.MNIST import MNIST
-from Datasets.FASHION import FASHION
-from Datasets.CIFAR10 import CIFAR10
 from Datasets.UNSW import UNSW
-from Datasets.Slicing5G import Slicing5G
 
 # models
 from Models.IOT_DNL_M1 import IOT_DNL_M1
 from Models.UNSW_M1 import UNSW_M1
-from Models.CIFAR10_M1 import CIFAR10_M1
-from Models.FASHION_M1 import FASHION_M1
 
 # ML
 from ML.Torch import Torch as MLtorch
@@ -37,17 +30,8 @@ from ML.Tensorflow import Tensorflow as MLtf
 from Comm.MPI import MPI as CommMPI
 
 # FL
-from FL.CentralizedAsync import CentralizedAsync
 from FL.CentralizedSync import CentralizedSync
-from FL.DecentralizedAsync import DecentralizedAsync
 from FL.DecentralizedSync import DecentralizedSync
-
-# XAI
-# from XAI.GradCAM import GradCAM
-# from XAI.IntegratedGrads import IntegratedGrads
-# from XAI.PDV import PDV
-# from XAI.PI import PI
-# from XAI.ALE import ALE
 
 # update tf logging level
 import tensorflow as tf
@@ -61,23 +45,16 @@ UTILS = {
     'model': ModelUtils,
     'ml': MLUtils,
     'fl': FLUtils,
-    'xai': XAIUtils
 }
 
 DATASETS = {
     'IOT_DNL': IOT_DNL,
-    'MNIST': MNIST,
-    'FASHION': FASHION,
-    'CIFAR10': CIFAR10,
     'UNSW': UNSW,
-    'Slicing5G': Slicing5G
 }
 
 MODELS = {
     'IOT_DNL': IOT_DNL_M1,
     'UNSW': UNSW_M1,
-    'CIFAR10': CIFAR10_M1,
-    'FASHION': FASHION_M1
 }
 
 ML = {
@@ -90,16 +67,6 @@ COMM = {
 }
 
 FL = {
-    'ca': CentralizedAsync,
     'cs': CentralizedSync,
-    'da': DecentralizedAsync,
     'ds': DecentralizedSync
 }
-
-# XAI = {
-#     'GradCAM': GradCAM,
-#     'IntegratedGrads': IntegratedGrads,
-#     'PDV': PDV,
-#     'PI': PI,
-#     'ALE': ALE
-# }
