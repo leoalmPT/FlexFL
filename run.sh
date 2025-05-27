6a727ee73dbdd725aa620d382166012ef1c08bce
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2025-05-27 11:53:51 +0100

    change unsw NN

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 bash scripts/run_on_vms_mpi.sh --min_workers 7 --learning_rate 0.0001 --patience 10 --fl ds --base_dir mpi_ds_1
-bash scripts/run_on_vms_mpi.sh --min_workers 7 --learning_rate 0.0001 --patience 10 --fl ds --base_dir mpi_ds_2
-bash scripts/run_on_vms_mpi.sh --min_workers 7 --learning_rate 0.0001 --patience 10 --fl ds --base_dir mpi_ds_3
-bash scripts/run_on_vms_mpi.sh --min_workers 7 --learning_rate 0.0001 --patience 10 --fl ds --base_dir mpi_ds_4
-bash scripts/run_on_vms_mpi.sh --min_workers 7 --learning_rate 0.0001 --patience 10 --fl ds --base_dir mpi_ds_5
-bash scripts/run_on_vms_mpi.sh --min_workers 7 --learning_rate 0.0001 --patience 10 --fl ds --base_dir mpi_ds_6
+
+bash scripts/run_on_vms.sh 0 0 --min_workers 7 --learning_rate 0.0001 --patience 10 --fl ds -c zenoh
